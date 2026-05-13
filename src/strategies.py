c61b9818b66df3f08d44fff4e858c019fe046172
tsmelo <ts.melo@unifesp.br>
2026-05-13 18:26:37 -0300

    correcao de erro de digitacao

--- src/strategies.py
+++ src/strategies.py
@@ -9,7 +9,7 @@ class PoliticaSimples(PoliticaDeReserva):
     def validar_reserva(self, nova_reserva, reservas_existentes):
         for reserva in reservas_existentes:
             if reserva.sala.id == nova_reserva.sala.id and reserva.horario == nova_reserva.horario:
-                return False, "[Conflito de reserva]: Sala {reserva.sala.id} já reservada para o horário {reserva.horario}."
+                return False, f"[Conflito de reserva]: Sala {reserva.sala.id} já reservada para o horário {reserva.horario}."
         return True, "Reserva autorizada."
     
 
